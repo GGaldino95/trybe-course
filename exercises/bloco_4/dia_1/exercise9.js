d38747634d0af3ccceee4aeff8a2643db0553b30
ggaldino95 <ggaldino95@gmail.com>
2020-11-25 19:39:07 -0300

    Completed task 9

--- exercises/bloco_4/dia_1/exercise9.js
+++ exercises/bloco_4/dia_1/exercise9.js
@@ -1,9 +1,9 @@
 let a = 15;
-let b = 11;
-let c = 7;
+let b = 10;
+let c = 6;
 let boolean = false;
 
-if (a % 2 == 0 || b % 2 == 0 || c % 2 == 0) {
+if (a % 2 != 0 || b % 2 != 0 || c % 2 != 0) {
     boolean = true;
     console.log(boolean);
 }
