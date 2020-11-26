aad2a1da9c4f775b570cc1998881c4ddb07f7ccc
ggaldino95 <ggaldino95@gmail.com>
2020-11-26 12:08:47 -0300

    Completed task 5

--- exercises/bloco_4/dia_2/exercise.js
+++ exercises/bloco_4/dia_2/exercise.js
@@ -1,14 +1,10 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27]
-let sum = 0;
+let higherValue = 0;
 
 for (let i = 0; i < numbers.length; i++) {
-    sum += numbers[i];
+    if (higherValue < numbers[i]) {
+        higherValue = numbers[i];
+    }
 }
 
-let avg = sum / numbers.length;
-
-if (avg > 20) {
-    console.log("Value greater than 20")
-} else {
-    console.log("Value less than or equal to 20")
-}
+console.log(higherValue);
