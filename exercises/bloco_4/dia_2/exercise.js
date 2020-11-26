a40193646af7a16c6acb692f0d5ef6e60d33e2a1
ggaldino95 <ggaldino95@gmail.com>
2020-11-26 12:15:17 -0300

    Completed task 6

--- exercises/bloco_4/dia_2/exercise.js
+++ exercises/bloco_4/dia_2/exercise.js
@@ -1,10 +1,14 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27]
-let higherValue = 0;
+let oddNumbers = 0;
 
 for (let i = 0; i < numbers.length; i++) {
-    if (higherValue < numbers[i]) {
-        higherValue = numbers[i];
+    if (numbers[i] % 2 != 0) {
+        oddNumbers++;
     }
 }
 
-console.log(higherValue);
+if (oddNumbers == 0) {
+    console.log("No odd numbers found")
+} else {
+    console.log(oddNumbers);
+}
