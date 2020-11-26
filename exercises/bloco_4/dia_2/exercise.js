1abf81419a7e000fbe079916da80e10766aedf06
ggaldino95 <ggaldino95@gmail.com>
2020-11-26 11:59:10 -0300

    Completed task 2

--- exercises/bloco_4/dia_2/exercise.js
+++ exercises/bloco_4/dia_2/exercise.js
@@ -1,5 +1,8 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27]
+let sum = 0;
 
 for (let i = 0; i < numbers.length; i++) {
-    console.log(numbers[i]);
+    sum += numbers[i];
 }
+
+console.log(sum);
