b2a77541cefa899346bb7f5edcbcff470a412336
ggaldino95 <ggaldino95@gmail.com>
2020-11-30 18:39:03 -0300

    Completed task 4 from part I

---
+++ exercises/bloco_4/dia_4/exercise01_4.js
@@ -0,0 +1,10 @@
+let info = {
+    personagem: "Margarida",
+    origem: "Pato Donald",
+    nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+};
+
+info.recorrente = "Sim";
+for (key in info) {
+    console.log(info[key]); // Showing only the properties values instead of 'name and value'.
+}
