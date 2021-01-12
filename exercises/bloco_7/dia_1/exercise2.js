db8a426d5356922637e9e07c3562516358789d12
ggaldino95 <ggaldino95@gmail.com>
2021-01-12 17:12:28 -0300

    [Task 2] Created function to find the longest word inside a string

--- exercises/bloco_7/dia_1/exercise2.js
+++ exercises/bloco_7/dia_1/exercise2.js
@@ -1,2 +1,23 @@
+// Parte 1
+console.log(`\n=============\nParte 1\n=============`);
+
 const fatorial = n => n <= 1 ? 1 : n * fatorial(n-1);
 console.log(fatorial(5));
+
+// Parte 2
+console.log(`\n=============\nParte 2\n=============`);
+
+const longestWord = string => {
+
+    const arrayString = string.split(' ');
+    let longestWord = '';
+
+    for (let i = 0; i < arrayString.length; i += 1) {
+        if (arrayString[i].length > longestWord.length) {
+            longestWord = arrayString[i];
+        }
+    }
+    return longestWord;
+};
+
+console.log(longestWord('Antônio foi no banheiro e não sabemos o que aconteceu'));
