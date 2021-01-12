59b2637f5736f550433c2951a32b0e0cba4cbf05
ggaldino95 <ggaldino95@gmail.com>
2021-01-12 17:00:40 -0300

    [Task 2] Created recursive Factorial function with ternary operator

--- exercises/bloco_7/dia_1/exercise2.js
+++ exercises/bloco_7/dia_1/exercise2.js
@@ -1,9 +1,5 @@
 function fatorial(n) {
-    if (n <= 1) {
-        return 1;
-    } else {
-        return n * fatorial(n-1);
-    }
+    return n <= 1 ? 1 : n * fatorial(n-1);
 }
 
-console.log(fatorial(5));
+console.log(fatorial(4));
