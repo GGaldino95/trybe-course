0c016765b75982759c4dcd0e22bd15321b63518b
ggaldino95 <ggaldino95@gmail.com>
2021-02-03 18:58:42 -0300

    Task 3 completed

--- exercises/bloco_9/dia_2/promise.js
+++ exercises/bloco_9/dia_2/promise.js
@@ -5,7 +5,14 @@ const promise = new Promise((resolve, reject) => {
     }
 
     const output = array.reduce((result, number) => result + Math.pow(number, 2));
-    return output > 8000 ? resolve() : reject();
+    return output > 8000 ? resolve(output) : reject();
 })
-    .then()
+    .then(output => {
+        const array = [output / 2, output / 3, output / 5, output / 10];
+        return console.log(`Succeso! O valor recebido foi: ${output}
+${output} dividido por 2: ${array[0].toFixed(1)}
+${output} dividido por 3: ${array[1].toFixed(1)}
+${output} dividido por 5: ${array[2].toFixed(1)}
+${output} dividido por 10: ${array[3].toFixed(1)}`);
+    })
     .catch();
