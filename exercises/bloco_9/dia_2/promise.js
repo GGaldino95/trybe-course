3707c69b641fa88eae9ba5dd01451ecef4cabc4f
ggaldino95 <ggaldino95@gmail.com>
2021-02-03 19:06:19 -0300

    Task 5 completed

--- exercises/bloco_9/dia_2/promise.js
+++ exercises/bloco_9/dia_2/promise.js
@@ -9,10 +9,18 @@ const promise = new Promise((resolve, reject) => {
 })
     .then(output => {
         const array = [output / 2, output / 3, output / 5, output / 10];
-        return console.log(`Succeso! O valor recebido foi: ${output}
+        console.log(`Succeso! O valor recebido foi: ${output}
 ${output} dividido por 2: ${array[0].toFixed(1)}
 ${output} dividido por 3: ${array[1].toFixed(1)}
 ${output} dividido por 5: ${array[2].toFixed(1)}
 ${output} dividido por 10: ${array[3].toFixed(1)}`);
+
+        return new Promise((resolve) => {
+            return resolve(array);
+        })
+            .then(array => {
+                const output = array.reduce((result, number) => result + number);
+                return console.log(`O somatorio das divisoes foi: ${output.toFixed(1)}`)
+            });
     })
     .catch(msg => console.log(msg));
