6eeb4edf22a0fdc74f793584359c94bc9fff1614
ggaldino95 <ggaldino95@gmail.com>
2021-02-04 14:47:53 -0300

    Generated Jest files for exercise testing

---
+++ exercises/bloco_10/dia_1/exercise1.js
@@ -0,0 +1,10 @@
+// Exercise 1 - Unit Tests
+const assert = require('assert');
+
+function sum(a, b) {
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    throw new Error('parameters must be numbers');
+  }
+
+  return a + b;
+}
