7c096b9627071f48053ed554436428f7ac18ed3a
ggaldino95 <ggaldino95@gmail.com>
2021-02-24 22:45:09 -0300

    Task 2 under development

--- exercises/bloco_12/dia_1/src/Pokedex.js
+++ exercises/bloco_12/dia_1/src/Pokedex.js
@@ -1,32 +1,63 @@
 import { Component } from 'react';
 import PropTypes from 'prop-types';
 import Pokemon from './Pokemon.js';
+import Button from './Button.js';
 
 class Pokedex extends Component {
     constructor() {
         super();
-        this.handleButton = this.handleButton.bind(this);
+        this.previousPokemon = this.previousPokemon.bind(this);
+        this.nextPokemon = this.nextPokemon.bind(this);
+        this.filterPokemon = this.filterPokemon.bind(this);
+        this.fetchFilteredPokemons = this.fetchFilteredPokemons.bind(this);
 
         this.state = {
-            pkmnIndex: 0
+            pkmnIndex: 0,
+            pkmnType: 'all'
         }
     };
 
-    handleButton() {
+    fetchFilteredPokemons() {
+        const { pokemonData } = this.props;
+        const filteredType = this.state;
+
+        return pokemonData.filter(currentPokemon => filteredType.pkmnType === 'all' ? true : currentPokemon.type === filteredType.pkmnType);
+    }
+
+    previousPokemon() {
+        this.setState((previousState, props) =>
+            previousState.pkmnIndex !== 0 ? { pkmnIndex: previousState.pkmnIndex -= 1 } : { pkmnIndex: props.pokemonData.length - 1 }
+        );
+    }
+
+    nextPokemon() {
         this.setState((previousState, props) =>
             previousState.pkmnIndex !== props.pokemonData.length - 1 ? { pkmnIndex: previousState.pkmnIndex += 1 } : { pkmnIndex: 0 }
         );
     };
 
+    filterPokemon(type) {
+        this.setState(() => ({
+            pkmnIndex: 0,
+            pkmnType: type
+        }));
+    }
+
     render() {
-        const { pokemonData } = this.props;
+        const filteredPokemons = this.fetchFilteredPokemons();
+        const pokemonData = filteredPokemons[this.state.pkmnIndex];
+        console.log(filteredPokemons)
+
         return (
             <main className="pokedex">
                 <h1>Pokedex</h1>
                 <section className="pokedex-container">
-                    <Pokemon key={pokemonData.id} pokemon={pokemonData[this.state.pkmnIndex]} />
+                    <button className="pokemon-button change-pokemon" onClick={this.previousPokemon}> {'<'} </button>
+                    <Pokemon key={pokemonData.id} pokemon={pokemonData} />
+                    <button className="pokemon-button change-pokemon" onClick={this.nextPokemon}> {'>'} </button>
                 </section>
-                <button className="pokemon-button next-pokemon" onClick={this.handleButton}>Next Pokemon</button>
+                <button className="pokemon-button pokemon-type fire" onClick={() => this.filterPokemon('Fire')}> {'Fire'} </button>
+                <button className="pokemon-button pokemon-type psychic" onClick={() => this.filterPokemon('Psychic')}> {'Psychic'} </button>
             </main>
         );
     };
