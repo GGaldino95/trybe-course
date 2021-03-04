62b44a6b676cc55d47567141f9e88bbd537c7bdc
ggaldino95 <ggaldino95@gmail.com>
2021-03-03 23:06:49 -0300

    Refactored Form and App components

--- exercises/bloco_12/dia_2/src/App.js
+++ exercises/bloco_12/dia_2/src/App.js
@@ -1,12 +1,66 @@
+import React from 'react';
 import './App.css';
 import Form from './components/Form';
+import Submit from './components/Submit';
+import FilledInformation from './components/FilledInformation';
 
-function App() {
-  return (
-    <main>
-      <Form />
-    </main>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.handleChanges = this.handleChanges.bind(this);
+    this.handleBlur = this.handleBlur.bind(this);
+    this.generateInfo = this.generateInfo.bind(this);
+
+    this.state = {
+      name: '',
+      email: '',
+      cpf: '',
+      address: '',
+      city: '',
+      state: '',
+      type: '',
+      summary: '',
+      position: '',
+      description: '',
+      submittedInfo: false
+    };
+  }
+
+  handleChanges({ target }) {
+    let { name, value } = target;
+    if (name === 'name') value = value.toUpperCase();
+    if (name === 'address' && value.match(/\W/)) value = value.replaceAll(/\W/g, '');
+
+    this.setState({ [name]: value });
+  };
+
+  handleBlur({ target }) {
+    let { name, value } = target;
+    this.setState(value.match(/\d/) ? { [name]: '' } : { [name]: value });
+  };
+
+  generateInfo(event) {
+    event.preventDefault();
+    this.setState({ submittedInfo: true });
+  }
+
+  render() {
+    const { submittedInfo } = this.state;
+
+    return (
+      <main id="main-content">
+        <Form
+          initialState={this.state}
+          handleChanges={this.handleChanges}
+          handleBlur={this.handleBlur}
+        />
+        <Submit
+          onClick={this.generateInfo}
+        />
+        { submittedInfo && <FilledInformation currentState={ this.state } /> }
+      </main>
+    );
+  }
 }
 
 export default App;
--- exercises/bloco_12/dia_2/src/components/Form.jsx
+++ exercises/bloco_12/dia_2/src/components/Form.jsx
@@ -5,47 +5,20 @@ import ProfessionalInfo from './ProfessionalInfo';
 class Form extends React.Component {
   constructor() {
     super();
-    this.handleChanges = this.handleChanges.bind(this);
-    this.handleBlur = this.handleBlur.bind(this);
     this.handleMouseEnter = this.handleMouseEnter.bind(this);
     sessionStorage.setItem('hasAlerted', 'no');
-
-    this.state = {
-      name: '',
-      email: '',
-      cpf: '',
-      address: '',
-      city: '',
-      state: '',
-      type: '',
-      summary: '',
-      position: '',
-      description: ''
-    };
   };
-
-  handleChanges({ target }) {
-    let { name, value } = target;
-    if (name === 'name') value = value.toUpperCase();
-    if (name === 'address' && value.match(/\W/)) value = value.replaceAll(/\W/g, '');
-
-    this.setState({ [name]: value });
-  };
-
-  handleBlur({ target }) {
-    let { name, value } = target;
-    this.setState(value.match(/\d/) ? { [name]: '' } : { [name]: value });
-  }
 
   handleMouseEnter() {
     if (sessionStorage.getItem('hasAlerted') === 'no') {
       alert('Fill in this information carefully.');
       sessionStorage.setItem('hasAlerted', 'yes');
-    }
-  }
+    };
+  };
 
   render() {
-    const { name, email, cpf, address, city, state, type, summary, position, description } = this.state
+    const { name, email, cpf, address, city, state, type, summary, position, description } = this.props.initialState;
+    const { handleChanges, handleBlur } = this.props 
     const personalData = { name, email, cpf, address, city, state, type };
     const professionalData = { summary, position, description };
 
@@ -53,17 +26,17 @@ class Form extends React.Component {
       <form>
         <PersonalInfo
           currentState={ personalData }
-          handleChanges={ this.handleChanges }
-          handleBlur={ this.handleBlur }
+          handleChanges={ handleChanges }
+          handleBlur={ handleBlur }
         />
         <ProfessionalInfo
           currentState={ professionalData }
-          handleChanges={ this.handleChanges }
+          handleChanges={ handleChanges }
           handleMouseEnter={ this.handleMouseEnter }
         />
       </form>
-    )
-  }
-}
+    );
+  };
+};
 
 export default Form;
