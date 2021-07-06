fd3f1fd6b5aba31f238d6c68330782d5f5334441
ggaldino95 <ggaldino95@gmail.com>
2021-07-06 16:19:42 -0300

    Bonus task completed

--- exercises/bloco_27/dia_3/models/connection.js
+++ exercises/bloco_27/dia_3/models/connection.js
@@ -1,9 +1,18 @@
-const mysql = require('mysql2/promise');
+const mongoClient = require('mongodb').MongoClient;
 
-const connection = mysql.createPool({
-  host: 'localhost', // Se necessário, substitua pelo seu host, `localhost` é o comum
-  user: 'gal', // Se necessário, substitua pelo seu usuário para conectar ao banco na sua máquina
-  password: '153426asd', // Se necessário, substitua pela sua senha para conectar ao banco na sua máquina
-  database: 'rest_exercicios'});
+const MONGO_DB_URL = 'mongodb://127.0.0.1:27017';
+
+const connection = () => {
+  return mongoClient
+    .connect(MONGO_DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then((conn) => conn.db('rest_exercicios'))
+    .catch((err) => {
+      console.error(err);
+      process.exit(1);
+   });
+};
 
 module.exports = connection;
--- exercises/bloco_27/dia_3/models/productModel.js
+++ exercises/bloco_27/dia_3/models/productModel.js
@@ -1,61 +1,40 @@
+const mongodb = require('mongodb');
 const connection = require('./connection');
+const { ObjectId } = require('mongodb');
 
-const add = async (name, brand) => {
-  try {
-    const [
-      result,
-    ] = await connection.query(
-      `INSERT INTO products (name, brand) VALUES (?, ?);`,
-      [name, brand]
-    );
-
-    return { id: result.insertId, name, brand };
-  } catch (err) {
-    console.error(err);
-    return process.exit(1);
+async function getAll() {
+    const db = await connection();
+    const products = await db.collection('products').find().toArray();
+    return products;
   }
-};
 
-const getAll = async () => {
-  try {
-    const [rows] = await connection.query('SELECT * FROM products');
-    return rows;
-  } catch (err) {
-    console.error(err);
-    return process.exit(1);
-  }
-};
+async function getById(id) {
+  const db = await connection();
+  if(!ObjectId.isValid(id)) return null;
+  return db.collection('products').findOne(ObjectId(id));
+}
 
-const getById = async (id) => {
-  try {
-    const [result] = await connection.query('SELECT * FROM products WHERE id = ?', [id]);
-    if (!result.length) return null
-    return result[0];
-  } catch (err) {
-    console.error(err);
-    return process.exit(1);
-  }
-};
+async function add(name, brand) {
+  const db = await connection();
+  const addProduct = await db.collection('products').insertOne({name, brand});
+  return addProduct;
+}
 
-const update = async (id, name, brand) => {
-  try {
-    await connection.query('UPDATE products SET name = ?, brand = ? WHERE id = ?', [name, brand, id])
-  } catch (err) {
-    console.error(err);
-    return process.exit(1);
-  }
-};
+async function exclude (id) {
+  const db = await connection();
+  if(!ObjectId.isValid(id)) return null;
+  const product = await getBytId(id);
+  await db.collection('products').deleteOne({ _id: ObjectID(id) });
+  return product;
+}
 
-const exclude = async (id) => {
-  try {
-    const product = await getById(id);
-    if (!product) return {};
-    await connection.query('DELETE FROM products WHERE id = ?', [id])
-    return product;
-  } catch (err) {
-    console.error(err);
-    return process.exit(1);
-  }
-};
+async function update(id) {
+  const db = await connection();
+  if(!ObjectId.isValid(id)) return null;
+  const product = await db.collection('products').updateOne({ _id: ObjectID(id) }, { $set: name, brand });
+  if(!product) return add(name, brand);
+  return product;
+}
+
 
 module.exports = { add, getAll, getById, update, exclude };
