bd8bbdc37e5c0a7fb37bd6bfec37ecbbe6786b4f
ggaldino95 <ggaldino95@gmail.com>
2021-09-16 18:36:09 -0300

    Bonus exercises completed

--- exercises/bloco_26/dia_1/index.js
+++ exercises/bloco_26/dia_1/index.js
@@ -4,6 +4,8 @@ const scripts = [
   { name: 'Calcular IMC', script: './imc.js' },
   { name: 'Calcular velocidade média', script: './velocidade.js' },
   { name: 'Jogo de adivinhação', script: './sorteio.js' },
+  { name: 'Calcular fatorial', script: './fatorial.js' },
+  { name: 'Exibir n números de fibonacci', script: './fibonacci.js' },
 ];
 
 let mensagem = scripts.map((script, index) => `${index + 1} - ${script.name}`);
