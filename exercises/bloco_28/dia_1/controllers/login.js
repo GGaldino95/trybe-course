0d7dd3a0728d36ba3be160709daedc4382cd7934
ggaldino95 <ggaldino95@gmail.com>
2021-12-07 14:04:43 -0300

    Exercise 02 completed successfully

--- exercises/bloco_28/dia_1/controllers/login.js
+++ exercises/bloco_28/dia_1/controllers/login.js
@@ -16,14 +16,25 @@ module.exports = async (req, res, next) => {
   // Caso ocorra erro na validação do Joi, passamos esse erro para o express, que chamará nosso middleware de erro
   if (error) return next(error);
 
+  // Se o login for admin e a senha estiver incorreta
+  if (req.body.username === 'admin' && req.body.password !== 's3nh4S3gur4???') {
+    const err = new Error('Invalid username or password'); // Criamos um novo objeto de erro
+    err.statusCode = 401; // Adicionamos o status `401 Unauthorized` ao erro
+    
+    return next(err); // Passmos o erro para o express, para que seja tratado pelo middleware de erro
+  }
+
+  // Definimos admin como true se username e password estiverem corretos
+  const admin = req.body.username === 'admin' && req.body.password === 's3nh4S3gur4???';
+
   const payload = {
     username: req.body.username,
-    admin: false,
+    admin, // Passamos a utilizar o valor da variável `admin` para determinar o valor do campo `admin` no payload do token
   };
 
   const token = jwt.sign(payload, JWT_SECRET, {
     expiresIn: '1h',
-  });
+  });git
 
   res.status(200).json({ token });
 };
