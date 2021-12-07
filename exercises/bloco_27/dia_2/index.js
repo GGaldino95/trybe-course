71bef77ee45f1a70d9b1b8422c0d39fd7d212df3
ggaldino95 <ggaldino95@gmail.com>
2021-12-07 12:59:11 -0300

    Exercise 02 completed successfully

--- exercises/bloco_27/dia_2/index.js
+++ exercises/bloco_27/dia_2/index.js
@@ -1,12 +1,22 @@
 require('dotenv').config(); // Carregamos as variáveis de ambiente
 const express = require('express');
+const bodyParser = require('body-parser');
+const Cep = require('./controllers/Cep');
+const errorMiddleware = require('./middlewares/error.js');
 
 const app = express(); // Criamos a aplicação do express
+app.use(bodyParser.json());
 
 // Registramos o endpoint `GET /ping`
 app.get('/ping', (req, res) => {
   res.status(200).json({ message: 'pong!' });
 });
+
+// Registramos o endpoint `GET /cep/:cep`
+app.get('/cep/:cep', Cep.findAddressByCep);
+
+// Registramos o middleware de erro
+app.use(errorMiddleware);
 
 // Lemos a porta da variável de ambiente, ou usamos 3000
 const PORT = process.env.PORT || 3000;
---
+++ exercises/bloco_27/dia_2/services/cep.js
@@ -0,0 +1,35 @@
+const Cep = require('../models/Cep');
+
+const CEP_REGEX = /\d{5}-?\d{3}/;
+
+const findAddressByCep = async (searchedCep) => {
+  // Valida o CEP e em caso dele ser falso, retorna uma erro
+  if (!CEP_REGEX.test(cep)) {
+    return {
+      error: {
+        code: 'invalidData',
+        message: 'CEP inválido',
+      }
+    }
+  }
+
+  // Buscamos o CEP através do Model
+  const cep = await Cep.findAddressByCep(searchedCep);
+
+  // Caso não econtre nenhum CEP, o service retorna um objeto de erro.
+  if (!cep) {
+    return {
+      error: {
+        code: 'notFound',
+        message: 'CEP não encontrado'
+      },
+    };
+  }
+
+  // Por fim, retornamos o CEP correto
+  return cep;
+};
+
+module.exports = {
+  findAddressByCep,
+};
