23d360ce8a0d3ad49e6d77739761d4021e1d27e4
ggaldino95 <ggaldino95@gmail.com>
2021-12-07 15:57:00 -0300

    Exercise 02 completed successfully

--- exercises/bloco_29/dia_1/index.js
+++ exercises/bloco_29/dia_1/index.js
@@ -2,11 +2,12 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Está vivo!!!')
-});
-
 const port = process.env.PORT || 3000;
+const message = process.env.MESSAGE || 'Está vivo!!!';
+
+app.get('/', (req, res) => {
+  res.send(message);
+});
 
 app.listen(port);
 console.log(`Escutando na porta ${port}`);
