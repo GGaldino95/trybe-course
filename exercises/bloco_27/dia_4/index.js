26fa2eee44084082ddc75288d1f861a58c67b8e9
ggaldino95 <ggaldino95@gmail.com>
2021-12-07 13:27:47 -0300

    Exercise 04 completed successfully

--- exercises/bloco_27/dia_4/index.js
+++ exercises/bloco_27/dia_4/index.js
@@ -7,6 +7,10 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.get('/movies', MovieController.getAll);
+
+app.get('/movies/:id', MovieController.findById);
+
 app.post('/movies', MovieController.create);
 
 const PORT = process.env.PORT || 3000;
