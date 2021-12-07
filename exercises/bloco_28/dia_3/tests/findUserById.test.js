b0fddf7c2f249690d8020b85c8448cb4108b36ad
ggaldino95 <ggaldino95@gmail.com>
2021-12-07 15:36:37 -0300

    Exercise 02 completed successfully

--- exercises/bloco_28/dia_3/tests/findUserById.test.js
+++ exercises/bloco_28/dia_3/tests/findUserById.test.js
@@ -37,5 +37,68 @@ describe('GET /api/users/:userId', () => {
       expect(response.body.error).to.be.equal('Token não encontrado ou informado');
     });
   });
+
+
+  describe('Quando o usuário solicita informações de outro usuário', () => {
+    let response;
+    const DBServer = new MongoMemoryServer();
+
+    before(async () => {
+      const connectionMock = await DBServer.getUri()
+        .then(URLMock => MongoClient.connect(
+          URLMock,
+          { useNewUrlParser: true, useUnifiedTopology: true }
+        ));
+
+      sinon.stub(MongoClient, 'connect')
+        .resolves(connectionMock);
+
+      connectionMock.db('jwt_exercise')
+        .collection('users')
+        .insertOne({
+          _id: EXAMPLE_ID,
+          username: 'fake-user',
+          password: 'fake-password',
+          name: 'fake-name',
+          birthdate: '01/01/1960',
+          biography: 'fake-biography',
+        })
+
+      const token = await chai.request(server)
+        .post('/api/login')
+        .send({
+          username: 'fake-user',
+          password: 'fake-password'
+        })
+        .then((res) => res.body.token);
+
+      const OTHER_EXAMPLE_ID = '565de6ded1ff223100cd6aa2'
+
+      response = await chai.request(server)
+        .get(`/api/users/${OTHER_EXAMPLE_ID}`)
+        .set('authorization', token);
+    });
+
+    after(async () => {
+      MongoClient.connect.restore();
+      await DBServer.stop();
+    });
+
+    it('retorna código de status "401"', () => {
+      expect(response).to.have.status(401);
+    });
+
+    it('retorna um objeto no body', () => {
+      expect(response.body).to.be.an('object');
+    });
+
+    it('objeto de resposta possui a propriedade "error"', () => {
+      expect(response.body).to.have.property('error');
+    });
+
+    it('a propriedade "error" possui a mensagem "Acesso negado"', () => {
+      expect(response.body.error).to.be.equal('Acesso negado');
+    });
+  });
 });
 
