3dcbba0bafd898b97487d7e1bea754f824fb72c5
ggaldino95 <ggaldino95@gmail.com>
2022-01-04 12:46:03 -0300

    Exercises completed successfully

---
+++ exercises/bloco_37/dia_4/exercise_02.py
@@ -0,0 +1,20 @@
+def longer_no_repeating_substring_len(string):
+    biggest = 0
+
+    for index, _ in enumerate(string):
+        substr = set()
+
+        for letter in string[index:]:
+            if letter in substr:
+                break
+
+            substr.add(letter)
+
+        if len(substr) > biggest:
+            biggest = len(substr)
+
+    return biggest
+
+
+str = "serdevemuitolegalmasehprecisoestudarbastante"
+print(longer_no_repeating_substring_len(str))
