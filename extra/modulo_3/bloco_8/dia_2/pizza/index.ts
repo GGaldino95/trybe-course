798c64d21f7aca624ae6677dfcf8ed5b8cb55712
ggaldino95 <ggaldino95@gmail.com>
2022-10-04 20:03:48 -0300

    Exercise 03 completed, folder restructuring

---
+++ extra/modulo_3/bloco_8/dia_2/pizza/index.ts
@@ -0,0 +1,23 @@
+import Pizza from './pizza';
+
+const calabresa: Pizza = {
+  flavor: 'Calabresa',
+  slices: 8
+}
+
+console.log(calabresa);
+
+const marguerita: Pizza = {
+  flavor: 'Marguerita',
+  slices: 6
+}
+
+console.log(marguerita);
+
+
+const nutela: Pizza = {
+  flavor: 'Nutela',
+  slices: 4
+}
+
+console.log(nutela);
