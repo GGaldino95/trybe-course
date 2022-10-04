18e485fb5576a42e39a3277ed064ba15f74ce05e
ggaldino95 <ggaldino95@gmail.com>
2022-10-04 20:01:59 -0300

    Exercise 02 completed

---
+++ extra/modulo_3/bloco_8/dia_2/index.ts
@@ -0,0 +1,28 @@
+import Car from './car';
+
+const gol = new Car('Volkswagen', 'prata', 4);
+
+gol.turnOn();
+gol.speedUp();
+gol.speedDown();
+gol.turn('esquerda');
+gol.speedUp();
+gol.speedDown();
+gol.turn('direita');
+gol.speedUp();
+gol.speedDown();
+gol.turn('direita');
+gol.speedDown();
+gol.stop();
+gol.speedUp();
+gol.speedDown();
+gol.turn('direita');
+gol.speedUp();
+gol.speedDown();
+gol.turn('esquerda');
+gol.speedUp();
+gol.speedDown();
+gol.turn('direita');
+gol.speedDown();
+gol.stop();
+gol.speedUp();
